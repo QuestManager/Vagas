e68d03f5dcb7c07679f8deeff410c085af4085b6
Cristiano Ascari <cristianoascari@gmail.com>
2018-05-26 17:31:59 -0300

    Created main routes.

--- front-end-challenge/code/StarWars/src/app/app-routing.module.ts
+++ front-end-challenge/code/StarWars/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+// Angular modules.
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Custom components.
+import { CharactersComponent } from './characters/characters.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+// Routes config.
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'characters/:id', component: CharactersComponent },
+  { path: '**', redirectTo: 'notfound' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
