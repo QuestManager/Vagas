acfdf9bbdb6c6fd451cc2f07aec2a3a487d72f59
Cristiano Ascari <cristianoascari@gmail.com>
2018-05-26 17:02:11 -0300

    New Http service.

---
+++ front-end-challenge/code/StarWars/src/app/_services/http/http.service.ts
@@ -0,0 +1,28 @@
+// Angular modules.
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+// Interfaces.
+import { IFilm, IFilmBasic } from '_interfaces/film.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  // Endpoints.
+  api: string = 'https://swapi.co/api/';
+  apiFilms: string = this.api + 'films/';
+  apiPeople: string = this.api + 'people/';
+  apiPlanets: string = this.api + 'planets/';
+  apiSpecies: string = this.api + 'species/';
+  apiStarships: string = this.api + 'starships/';
+  apiVehicles: string = this.api + 'vehicles/';
+
+  // Constructor method.
+  constructor() { }
+
+
+
+}
