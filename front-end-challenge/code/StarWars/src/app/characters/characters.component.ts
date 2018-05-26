4903fea4c56b4cc553c48e448cc0e052a1886558
Cristiano Ascari <cristianoascari@gmail.com>
2018-05-26 17:31:09 -0300

    New Characters component.

--- front-end-challenge/code/StarWars/src/app/characters/characters.component.ts
+++ front-end-challenge/code/StarWars/src/app/characters/characters.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+// Angular modules.
+import { ActivatedRoute, Router, RouterLinkActive } from '@angular/router';
+import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
   styleUrls: ['./characters.component.scss']
 })
-export class CharactersComponent implements OnInit {
+export class CharactersComponent implements OnDestroy, OnInit {
 
-  constructor() { }
+  // Class of current body.
+  bodyClass: string = 'characters_page';
 
-  ngOnInit() {
+  // Constructor method.
+  constructor(
+    private renderer: Renderer2,
+    private route: ActivatedRoute
+  ) {
+
+    // Change <body> class.
+    this.renderer.addClass(document.body, this.bodyClass);
+
+    // Get item ID from route parameters.
+    this.route.params.subscribe(params => {
+
+      // Get product ID.
+      if (params['id']) {
+
+      }
+
+    });
+
   }
+
+  // When component is destroyed.
+  public ngOnDestroy() {
+
+    // Remove class from <body>.
+    this.renderer.removeClass(document.body, this.bodyClass);
+
+  }
+
+  // On init.
+  ngOnInit() {}
 
 }
